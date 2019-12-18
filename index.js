d7f2aea42bac4f5f5f0d99ef933ddb906282323c
LESANF <54767632+LESANF@users.noreply.github.com>
2019-12-18 16:12:53 +0900

    docs: Edit description

--- index.js
+++ index.js
@@ -7,4 +7,17 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  console.log(req);
+  res.send("hihihihihihihi this is res.send");
+}
+
+function handleProfile(req, res) {
+  res.send("this is profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
