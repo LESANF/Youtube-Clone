45804b3826e01bc360db5b404f18331b7f26037d
LESANF <nagong1000@naver.com>
2019-12-20 01:37:27 +0900

    docs: Edit  index.js, package.json, #2 ExpressJS(2, Start #2.6).txt

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const handleHome = (req, res) => res.send("hi i'm home ! change !");
 
 const handleProfile = (req, res) => res.send("this is profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
 app.get("/", handleHome);
+
+app.use(betweenHome);
 
 app.get("/profile", handleProfile);
 
