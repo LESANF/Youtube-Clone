0841a31f8d06c13c45d2ddd1d64fb5d3a0090211
LESANF <54767632+LESANF@users.noreply.github.com>
2019-12-20 14:51:16 +0900

    docs: Edit End #2.7

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,14 +15,13 @@ const handleHome = (req, res) => res.send("hi i'm home ! change !");
 
 const handleProfile = (req, res) => res.send("this is profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
-
-app.use(betweenHome);
 
 app.get("/profile", handleProfile);
 
