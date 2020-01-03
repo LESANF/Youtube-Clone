a267ea38662da88a429ae1ba005e73890f2405c3
LESANF <nagong1000@naver.com>
2020-01-03 11:44:57 +0900

    docs: End #2.22

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -10,7 +10,7 @@ export const search = (req, res) => {
     query: { term: searchingBy }
   } = req;
 
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) =>
