4ab8027fd22dcd528b1d3e92c9663b277ae62d11
LESANF <nagong1000@naver.com>
2020-01-05 04:58:25 +0900

    docs: End #2.24

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export function localMiddleware(req, res, next) {
   res.locals.siteName = "Youtube";
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 999
+  };
   next();
 }
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -3,9 +3,10 @@ import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
   getJoin,
-  login,
-  logout,
-  postJoin
+  postJoin,
+  getLogin,
+  postLogin,
+  logout
 } from "../controllers/userController";
 
 const globalRouter = express.Router();
@@ -14,9 +15,11 @@ const globalRouter = express.Router();
 globalRouter.get(routes.join, getJoin);
 globalRouter.post(routes.join, postJoin);
 
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
 export default globalRouter;
