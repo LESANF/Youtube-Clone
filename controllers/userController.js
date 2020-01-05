bbc27c0c32ff14411b0ca7985640430a5457877d
LESANF <nagong1000@naver.com>
2020-01-06 00:48:10 +0900

    docs: END #2 ExpressJS

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,8 +26,11 @@ export const postLogin = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Log Out" });
+export const logout = (req, res) => {
+  //To do : Process Log Out
+  res.redirect(routes.home);
+};
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "../routes";
 import {
   videos,
-  upload,
+  getUpload,
+  postUpload,
   videoDetail,
   editVideo,
   deleteVideo
@@ -10,8 +11,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
