1ce8d8be65b0a859a34c34455b024596714a83b1
LESANF <nagong1000@naver.com>
2020-01-09 19:40:56 +0900

    docs: #3.5 End

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,16 +2,13 @@ import routes from "../routes";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  throw Error("This is error from throw !");
-  res.render("home", { pageTitle: "Home", videos });
-  // try {
-  //   const videos = await Video.find({});
-  //   res.render("home", { pageTitle: "Home", videos });
-  // } catch (error) {
-  //   console.log(error);
-  //   res.render("home", { pageTitle: "Home", videos: [] });
-  // }
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
