789abb107690ee42f698d433e3328073b0b748cc
LESANF <nagong1000@naver.com>
2020-01-22 06:46:38 +0900

    docs: #6.5 END

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -8,15 +8,16 @@ import {
   getLogin,
   postLogin
 } from "../controllers/userController";
+import { onlyPublic } from "../middlewares";
 
 const globalRouter = express.Router();
 
 // get, post
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
