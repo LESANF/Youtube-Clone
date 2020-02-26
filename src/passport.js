25ed25054bb9f54b8512464dc6b9f3f947ecab00
LESANF <nagong1000@naver.com>
2020-02-26 21:12:22 +0900

    Github Passport

--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `https://tranquil-journey-66023.herokuapp.com${routes.githubCallback}`
     },
     githubLoginCallback
   )
